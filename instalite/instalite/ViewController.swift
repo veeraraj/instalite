0996bd5eda0bcc95f71b385018479ed2f15f1220
Veeraraj Veeramani <veeraraj.v@gmail.com>
2022-12-10 16:02:58 +0100

    [EUS-0000]: Added xCode project

---
+++ instalite/instalite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instalite
+//
+//  Created by Veera on 10/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
